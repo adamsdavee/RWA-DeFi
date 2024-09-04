4f4a062a1793b4c3c15fecaf2289a1f9dc1f85dd
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-09-04 20:14:51 +0100

    Fix styling

--- frontend/src/components/TokenCard.jsx
+++ frontend/src/components/TokenCard.jsx
@@ -13,34 +13,34 @@ const TokenCard = ({ price, mktCap, circulating, name, utility, details }) => {
 			<p>{details}</p>
 
 			<div className="py-5">
-				<button className="px-5 py-2 text-black font-semibold bg-white rounded-3xl hover:opacity-75 mr-4">
+				<button className="px-5 py-2 text-black font-semibold bg-white rounded-3xl hover:opacity-75 mr-4 duration-200 md:hover:rounded-xl">
 					Learn More
 				</button>
-				<button className="px-5 py-2 white rounded-3xl hover:bg-white hover:text-black border border-white">
+				<button className="px-5 py-2 white rounded-3xl border border-white duration-200 md:hover:rounded-xl">
 					Buy Now
 				</button>
 			</div>
 
 			{/* Minicards */}
 			<div className="flex flex-row gap-2 justify-between py-4">
-				<div className="rounded-lg bg-white backdrop-blur-xl p-5 pr-5 bg-opacity-20 flex flex-row gap-2 items-center">
-					<AiOutlineDollarCircle className="text-2xl"/>
+				<div className="rounded-lg bg-white backdrop-blur-xl p-2 md:p-5 md:pr-3 bg-opacity-20 flex flex-row gap-2 items-center">
+					<AiOutlineDollarCircle className="text-2xl hidden md:block" />
 					<div className="flex flex-col gap-2 items-center">
 						<span className="font-bold text-lg">{price}</span>
-						<span >Price</span>
+						<span>Price</span>
 					</div>
 				</div>
 
-				<div className="rounded-lg bg-white backdrop-blur-xl p-5 pr-5 bg-opacity-20 flex flex-row gap-2 items-center ">
-					<AiOutlineDollarCircle className="text-2xl"/>
+				<div className="rounded-lg bg-white backdrop-blur-xl p-2 md:p-5 md:pr-3 bg-opacity-20 flex flex-row gap-2 items-center ">
+					<AiOutlineDollarCircle className="text-2xl hidden md:block" />
 					<div className="flex flex-col gap-2 items-center">
 						<span className="font-bold text-lg">{mktCap}</span>
 						<span>Mkt cap</span>
 					</div>
 				</div>
 
-				<div className="rounded-lg bg-white backdrop-blur-xl p-5 pr-5 bg-opacity-20 flex flex-row gap-2 items-center">
-					<AiOutlineDollarCircle className="text-2xl"/>
+				<div className="rounded-lg bg-white backdrop-blur-xl p-2 md:p-5 md:pr-3 bg-opacity-20 flex flex-row gap-2 items-center">
+					<AiOutlineDollarCircle className="text-2xl hidden md:block" />
 					<div className="flex flex-col gap-2 items-center">
 						<span className="text-lg font-bold">{circulating}</span>
 						<span>Circulating</span>
